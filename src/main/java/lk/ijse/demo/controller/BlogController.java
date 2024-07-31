c91ecceac2945bb1a7df7cd523653faa569d9426
kavindumal <kavindu11250403@gmail.com>
2024-07-31 13:41:47 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/demo/controller/BlogController.java
@@ -0,0 +1,41 @@
+package lk.ijse.demo.controller;
+
+import lk.ijse.demo.entity.Blog;
+import lk.ijse.demo.repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+    // End Point
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @PostMapping("/number")
+    public int getNumber() {
+        return 9;
+    }
+
+    @GetMapping("/data/{number}")
+    public int getData(@PathVariable int number) {
+        return number;
+    }
+
+    @GetMapping("/object")
+    public Blog getObject() {
+        Blog blog = new Blog();
+        return blog;
+    }
+
+    @PostMapping("/savepost")
+    public void savePost(@RequestBody Blog blog) {
+        blogRepository.save(blog);
+    }
+}
