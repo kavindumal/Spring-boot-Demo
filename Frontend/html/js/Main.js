e58031a11df484986a92b1382cc225ac7fc68d3b
kavindumal <kavindu11250403@gmail.com>
2024-08-07 11:18:15 +0530

    values send and saved successfully to database.

--- Frontend/html/js/Main.js
+++ Frontend/html/js/Main.js
@@ -5,4 +5,23 @@ $('#savepost').click(function () {
     let postContent=$('#postContent').val();
     let postCategory=$('#postCategory').val();
     console.log(postId,postTitle,postContent,postCategory);
+    $.ajax({
+        url:'http://localhost:8080/blog/savepost',
+        method:'POST',
+        contentType:'application/json',
+        "data":JSON.stringify({
+            "id":postId,
+            "title":postTitle,
+            "content":postTitle,
+            "category":postCategory,
+        }),
+        success:function (result){
+            console.log(result)
+            alert("Done")
+        },
+        error:function (error){
+            console.log(error)
+            alert("Try again later")
+        }
+    })
 })
