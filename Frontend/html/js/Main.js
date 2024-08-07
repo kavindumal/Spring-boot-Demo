79841a3914d47112d56ef6709a742e49068d01d9
kavindumal <kavindu11250403@gmail.com>
2024-08-07 11:24:52 +0530

    values send and updated successfully to database.

--- Frontend/html/js/Main.js
+++ Frontend/html/js/Main.js
@@ -25,3 +25,31 @@ $('#savepost').click(function () {
         }
     })
 })
+
+$('#updatepost').click(function () {
+    let postId=$('#postId').val();
+    let postTitle=$('#postTitle').val();
+    let postContent=$('#postContent').val();
+    let postCategory=$('#postCategory').val();
+    console.log(postId,postTitle,postContent,postCategory);
+    $.ajax({
+        url:'http://localhost:8080/blog/updatepost',
+        method:'Put',
+        contentType:'application/json',
+        "data":JSON.stringify({
+            "id":postId,
+            "title":postTitle,
+            "content":postTitle,
+            "category":postCategory,
+        }),
+        success:function (result){
+            console.log(result)
+            alert("Update Successfully")
+        },
+        error:function (error){
+            console.log(error)
+            alert("Try again")
+        }
+    })
+
+})
